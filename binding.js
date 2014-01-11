20e761d2b594fb01dfbe01dd30a2238cd1739e23
bredele <olivier.wietrich@gmail.com>
2014-01-11 13:12:49 -0700

    update standalone version

--- binding.js
+++ binding.js
@@ -375,6 +375,479 @@ module.exports = function(str) {
   return results;
  };
 });
+require.register("component-emitter/index.js", function(exports, require, module){
+
+/**
+ * Expose `Emitter`.
+ */
+
+module.exports = Emitter;
+
+/**
+ * Initialize a new `Emitter`.
+ *
+ * @api public
+ */
+
+function Emitter(obj) {
+  if (obj) return mixin(obj);
+};
+
+/**
+ * Mixin the emitter properties.
+ *
+ * @param {Object} obj
+ * @return {Object}
+ * @api private
+ */
+
+function mixin(obj) {
+  for (var key in Emitter.prototype) {
+    obj[key] = Emitter.prototype[key];
+  }
+  return obj;
+}
+
+/**
+ * Listen on the given `event` with `fn`.
+ *
+ * @param {String} event
+ * @param {Function} fn
+ * @return {Emitter}
+ * @api public
+ */
+
+Emitter.prototype.on =
+Emitter.prototype.addEventListener = function(event, fn){
+  this._callbacks = this._callbacks || {};
+  (this._callbacks[event] = this._callbacks[event] || [])
+    .push(fn);
+  return this;
+};
+
+/**
+ * Adds an `event` listener that will be invoked a single
+ * time then automatically removed.
+ *
+ * @param {String} event
+ * @param {Function} fn
+ * @return {Emitter}
+ * @api public
+ */
+
+Emitter.prototype.once = function(event, fn){
+  var self = this;
+  this._callbacks = this._callbacks || {};
+
+  function on() {
+    self.off(event, on);
+    fn.apply(this, arguments);
+  }
+
+  on.fn = fn;
+  this.on(event, on);
+  return this;
+};
+
+/**
+ * Remove the given callback for `event` or all
+ * registered callbacks.
+ *
+ * @param {String} event
+ * @param {Function} fn
+ * @return {Emitter}
+ * @api public
+ */
+
+Emitter.prototype.off =
+Emitter.prototype.removeListener =
+Emitter.prototype.removeAllListeners =
+Emitter.prototype.removeEventListener = function(event, fn){
+  this._callbacks = this._callbacks || {};
+
+  // all
+  if (0 == arguments.length) {
+    this._callbacks = {};
+    return this;
+  }
+
+  // specific event
+  var callbacks = this._callbacks[event];
+  if (!callbacks) return this;
+
+  // remove all handlers
+  if (1 == arguments.length) {
+    delete this._callbacks[event];
+    return this;
+  }
+
+  // remove specific handler
+  var cb;
+  for (var i = 0; i < callbacks.length; i++) {
+    cb = callbacks[i];
+    if (cb === fn || cb.fn === fn) {
+      callbacks.splice(i, 1);
+      break;
+    }
+  }
+  return this;
+};
+
+/**
+ * Emit `event` with the given args.
+ *
+ * @param {String} event
+ * @param {Mixed} ...
+ * @return {Emitter}
+ */
+
+Emitter.prototype.emit = function(event){
+  this._callbacks = this._callbacks || {};
+  var args = [].slice.call(arguments, 1)
+    , callbacks = this._callbacks[event];
+
+  if (callbacks) {
+    callbacks = callbacks.slice(0);
+    for (var i = 0, len = callbacks.length; i < len; ++i) {
+      callbacks[i].apply(this, args);
+    }
+  }
+
+  return this;
+};
+
+/**
+ * Return array of callbacks for `event`.
+ *
+ * @param {String} event
+ * @return {Array}
+ * @api public
+ */
+
+Emitter.prototype.listeners = function(event){
+  this._callbacks = this._callbacks || {};
+  return this._callbacks[event] || [];
+};
+
+/**
+ * Check if this emitter has `event` handlers.
+ *
+ * @param {String} event
+ * @return {Boolean}
+ * @api public
+ */
+
+Emitter.prototype.hasListeners = function(event){
+  return !! this.listeners(event).length;
+};
+
+});
+require.register("bredele-each/index.js", function(exports, require, module){
+
+/**
+ * Expose 'each'
+ */
+
+module.exports = function(obj, fn, scope){
+  if( obj instanceof Array) {
+    array(obj, fn, scope);
+  } else if(typeof obj === 'object') {
+    object(obj, fn, scope);
+  }
+};
+
+
+/**
+ * Object iteration.
+ * @param  {Object}   obj   
+ * @param  {Function} fn    
+ * @param  {Object}   scope 
+ * @api private
+ */
+
+function object(obj, fn, scope) {
+  for (var i in obj) {
+    if (obj.hasOwnProperty(i)) {
+      fn.call(scope, i, obj[i]);
+    }
+  }
+}
+
+
+/**
+ * Array iteration.
+ * @param  {Array}   obj   
+ * @param  {Function} fn    
+ * @param  {Object}   scope 
+ * @api private
+ */
+
+function array(obj, fn, scope){
+  for(var i = 0, l = obj.length; i < l; i++){
+    fn.call(scope, i, obj[i]);
+  }
+}
+});
+require.register("bredele-clone/index.js", function(exports, require, module){
+
+/**
+ * Expose 'clone'
+ * @param  {Object} obj 
+ * @api public
+ */
+
+module.exports = function(obj) {
+  if(obj instanceof Array) {
+    return obj.slice(0);
+  }
+  return clone(obj);
+};
+
+
+/**
+ * Clone object.
+ * @param  {Object} obj 
+ * @api private
+ */
+
+function clone(obj){
+  if(typeof obj === 'object') {
+    var copy = {};
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        copy[key] = clone(obj[key]);
+      }
+    }
+    return copy;
+  }
+  return obj;
+}
+});
+require.register("bredele-store/index.js", function(exports, require, module){
+var Emitter = require('emitter'),
+    clone = require('clone'),
+    each = require('each'),
+    storage = window.localStorage;
+
+/**
+ * Expose 'Store'
+ */
+
+module.exports = Store;
+
+
+/**
+ * Store constructor
+ * @api public
+ */
+
+function Store(data) {
+  if(data instanceof Store) return data;
+  this.data = data || {};
+  this.formatters = {};
+}
+
+
+Emitter(Store.prototype);
+
+/**
+ * Set store attribute.
+ * @param {String} name
+ * @param {Everything} value
+ * @api public
+ */
+
+Store.prototype.set = function(name, value, plugin) { //add object options
+  var prev = this.data[name];
+  if(prev !== value) {
+    this.data[name] = value;
+    this.emit('change', name, value, prev);
+    this.emit('change ' + name, value, prev);
+  }
+};
+
+
+/**
+ * Get store attribute.
+ * @param {String} name
+ * @return {Everything}
+ * @api public
+ */
+
+Store.prototype.get = function(name) {
+  var formatter = this.formatters[name];
+  var value = this.data[name];
+  if(formatter) {
+    value = formatter[0].call(formatter[1], value);
+  }
+  return value;
+};
+
+/**
+ * Get store attribute.
+ * @param {String} name
+ * @return {Everything}
+ * @api private
+ */
+
+Store.prototype.has = function(name) {
+  //NOTE: I don't know if it should be public
+  return this.data.hasOwnProperty(name);
+};
+
+
+/**
+ * Delete store attribute.
+ * @param {String} name
+ * @return {Everything}
+ * @api public
+ */
+
+Store.prototype.del = function(name) {
+  //TODO:refactor this is ugly
+  if(this.has(name)){
+    if(this.data instanceof Array){
+      this.data.splice(name, 1);
+    } else {
+      delete this.data[name]; //NOTE: do we need to return something?
+    }
+    this.emit('deleted', name, name);
+    this.emit('deleted ' + name, name);
+  }
+};
+
+
+/**
+ * Set format middleware.
+ * Call formatter everytime a getter is called.
+ * A formatter should always return a value.
+ * @param {String} name
+ * @param {Function} callback
+ * @param {Object} scope
+ * @return this
+ * @api public
+ */
+
+Store.prototype.format = function(name, callback, scope) {
+  this.formatters[name] = [callback,scope];
+  return this;
+};
+
+
+/**
+ * Compute store attributes
+ * @param  {String} name
+ * @return {Function} callback                
+ * @api public
+ */
+
+Store.prototype.compute = function(name, callback) {
+  //NOTE: I want something clean instaead passing the computed 
+  //attribute in the function
+  var str = callback.toString();
+  var attrs = str.match(/this.[a-zA-Z0-9]*/g);
+
+  this.set(name, callback.call(this.data)); //TODO: refactor (may be use replace)
+  for(var l = attrs.length; l--;){
+    this.on('change ' + attrs[l].slice(5), function(){
+      this.set(name, callback.call(this.data));
+    });
+  }
+};
+
+
+/**
+ * Reset store
+ * @param  {Object} data 
+ * @api public
+ */
+
+Store.prototype.reset = function(data) {
+  var copy = clone(this.data),
+      length = data.length;
+  this.data = data;
+
+
+    //remove undefined attributes
+    //TODO: we don't need to go through each items for array (only difference)
+    each(copy, function(key, val){
+      if(typeof data[key] === 'undefined'){
+        this.emit('deleted', key, length);
+        this.emit('deleted ' + key, length);
+      }
+    }, this);
+
+  //set new attributes
+  each(data, function(key, val){
+    //TODO:refactor with this.set
+    var prev = copy[key];
+    if(prev !== val) {
+      this.emit('change', key, val, prev);
+      this.emit('change ' + key, val, prev);
+    }
+  }, this);
+};
+
+
+/**
+ * Loop through store data.
+ * @param  {Function} cb   
+ * @param  {[type]}   scope 
+ * @api public
+ */
+
+Store.prototype.loop = function(cb, scope) {
+  each(this.data, cb, scope || this);
+};
+
+
+/**
+ * Synchronize with local storage.
+ * 
+ * @param  {String} name 
+ * @param  {Boolean} bool save in localstore
+ * @api public
+ */
+
+Store.prototype.local = function(name, bool) {
+  //TODO: should we do a clear for .local()?
+  if(!bool) {
+    storage.setItem(name, this.toJSON());
+  } else {
+    this.reset(JSON.parse(storage.getItem(name)));
+  }
+  //TODO: should we return this?
+};
+
+
+/**
+ * Use middlewares to extend store.
+ * A middleware is a function with the store
+ * as first argument.
+ * 
+ * @param  {Function} fn 
+ * @return {this}
+ * @api public
+ */
+
+Store.prototype.use = function(fn) {
+  fn(this);
+  return this;
+};
+
+
+/**
+ * Stringify model
+ * @return {String} json
+ * @api public
+ */
+
+Store.prototype.toJSON = function() {
+  return JSON.stringify(this.data);
+};
+
+//TODO: localstorage middleware like
+});
 require.register("component-indexof/index.js", function(exports, require, module){
 module.exports = function(arr, obj){
   if (arr.indexOf) return arr.indexOf(obj);
@@ -386,6 +859,7 @@ module.exports = function(arr, obj){
 });
 require.register("binding/index.js", function(exports, require, module){
 var subs = require('./lib/attr'),
+    Store = require('store'),
     parser = require('plugin-parser');
 
 
@@ -403,8 +877,8 @@ module.exports = Binding;
 
 function Binding(model){
   if(!(this instanceof Binding)) return new Binding(model);
-  //TODO: mixin with store if not instanceof store
-  this.model = model;
+  //TODO: remove store of dependencies
+  this.model = new Store(model);
   this.plugins = {};
 }
 
@@ -540,6 +1014,9 @@ function getProps(text) {
 
 
 
+
+
+
 require.alias("bredele-supplant/index.js", "binding/deps/supplant/index.js");
 require.alias("bredele-supplant/lib/props.js", "binding/deps/supplant/lib/props.js");
 require.alias("bredele-supplant/index.js", "binding/deps/supplant/index.js");
@@ -554,6 +1031,18 @@ require.alias("bredele-plugin-parser/index.js", "binding/deps/plugin-parser/inde
 require.alias("bredele-plugin-parser/index.js", "binding/deps/plugin-parser/index.js");
 require.alias("bredele-plugin-parser/index.js", "plugin-parser/index.js");
 require.alias("bredele-plugin-parser/index.js", "bredele-plugin-parser/index.js");
+require.alias("bredele-store/index.js", "binding/deps/store/index.js");
+require.alias("bredele-store/index.js", "binding/deps/store/index.js");
+require.alias("bredele-store/index.js", "store/index.js");
+require.alias("component-emitter/index.js", "bredele-store/deps/emitter/index.js");
+
+require.alias("bredele-each/index.js", "bredele-store/deps/each/index.js");
+require.alias("bredele-each/index.js", "bredele-store/deps/each/index.js");
+require.alias("bredele-each/index.js", "bredele-each/index.js");
+require.alias("bredele-clone/index.js", "bredele-store/deps/clone/index.js");
+require.alias("bredele-clone/index.js", "bredele-store/deps/clone/index.js");
+require.alias("bredele-clone/index.js", "bredele-clone/index.js");
+require.alias("bredele-store/index.js", "bredele-store/index.js");
 require.alias("component-indexof/index.js", "binding/deps/indexof/index.js");
 require.alias("component-indexof/index.js", "indexof/index.js");
 
