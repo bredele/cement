5e0fe3b445b74ec5b0acdbf505bf7ff35e5c64d0
bredele <olivier.wietrich@gmail.com>
2015-11-28 18:14:21 -0700

    refactor cement

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ var walk = require('domwalk');
  * Expose 'Cement'
  */
 
-module.exports = Cement;
+module.exports = function() {
+  return new Cement();
+};
 
 
 /**
@@ -19,51 +21,16 @@ module.exports = Cement;
  */
 
 function Cement() {
-  this.bindings = {};
+
 }
 
 
-/**
- * Bind node attribute with
- * function.
- * 
- * @param  {String} name 
- * @param  {Function} binding
- * @api public
- */
 
-Cement.prototype.bind = function(name, binding) {
-  this.bindings[name] = binding;
+Cement.prototype.attr = function() {
+
 };
 
 
-/**
- * Apply bindings on dom
- * element (root).
- *
- * A mandatory callback (text) allows
- * you to get the content of every text
- * node and transform it.
- * 
- * @param  {Element} root 
- * @param {Function} text 
- * @api public
- */
+Cement.prototype.render = function() {
 
-Cement.prototype.render = function(root, text) {
-  var bindings = this.bindings;
-  walk(root, function(node) {
-    var type = node.nodeType;
-    if(type === 1) {
-      var attrs = node.attributes;
-      for(var i = 0, l = attrs.length; i < l; i++) {
-        var attr = attrs[i];
-        var plugin = bindings[attr.nodeName];
-        if(plugin) plugin(node, attr.nodeValue);
-        else text(attr.nodeValue, attr);
-      }
-    } else {
-      text(node.nodeValue, node);
-    }
-  });
 };
--- test/cement.js
+++ test/cement.js
@@ -4,83 +4,16 @@
  */
 
 var assert = require('assert');
-var Cement = require('..');
+var cement = require('..');
 
 
-describe("cement binding", function() {
+describe('API', function() {
 
-  describe("text nodes", function() {
 
-    var cement;
-    beforeEach(function() {
-      cement = new Cement();
-    });
-
-    it("should bind text node", function(done) {
-      var node = domify('<section>hello</section>');
-      cement.render(node, function(str, el) {
-        if(str === 'hello') done();
-      });
-    });
-    
-    it("should bind attributes content", function(done) {
-      var node = domify('<section class="hello">');
-      cement.render(node, function(str, el) {
-        if(str === 'hello') done();
-      });
-    });
-    
+  it("should have the following API", function() {
+    var ui = cement();
+    assert(ui.attr);
+    assert(ui.render);
   });
 
-
-  describe("attribute nodes", function() {
-
-    var node, cement, text;
-    beforeEach(function() {
-      cement = new Cement();
-      node = domify('<section class="section" data-custom="something">');
-      text = function() {};
-    });
-    
-    it("should bind existing attribute", function(done) {
-      cement.bind('class', function(el, value) {
-        if(value === 'section') done();
-      });
-      cement.render(node, text);
-    });
-
-    it("should pass the target node", function(done) {
-      cement.bind('class', function(el, value) {
-        if(el === node) done();
-      });
-      cement.render(node, text);
-    });
-    
-    
-    it("should bind custom attribute", function(done) {
-      cement.bind('data-custom', function(el, value) {
-        if(value === 'something') done();
-      });
-      cement.render(node, text);
-    });
-
-  });
-
-  
-  
 });
-
-
-/**
- * Create dom from string.
- * 
- * @param  {String} text
- * @return {Element}
- */
-
-function domify(text) {
-  var div = document.createElement('div');
-  div.innerHTML = text;
-  return div.firstChild;
-}
-
